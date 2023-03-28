eb249664ff72892e0b3175a80eb9c0b9a50f2a58
Sudipa54 <sudipa6054@gmail.com>
2023-03-27 20:29:06 -0400

    added changes for M2

--- main.go
+++ main.go
@@ -8,28 +8,6 @@ import (
 	"movie-suggestions-api/utils/log"
 )
 
-// func connectToMongodb(l *log.Logger) {
-// 	client, err := mongo.NewClient(options.Client().ApplyURI(config.ATLAS_URI))
-// 	if err != nil {
-// 		l.Fatal(err)
-// 	}
-// 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-// 	err = client.Connect(ctx)
-// 	if err != nil {
-// 		l.Fatal(err)
-// 	}
-// 	defer client.Disconnect(ctx)
-
-// 	/*
-// 		List databases
-// 	*/
-// 	databases, err := client.ListDatabaseNames(ctx, bson.M{})
-// 	if err != nil {
-// 		l.Fatal(err)
-// 	}
-// 	fmt.Println(databases)
-// }
-
 func main() {
 
 	l := log.NewLogger("")
